823c26576b219959cea9ddede6768ffa020add8a
TheodoreBillotte <theodore.billotte@epitech.eu>
2025-07-31 19:04:56 +0200

    add: enhance movement component with grid-based positioning and animation

--- actors/player/components/movement_component.gd
+++ actors/player/components/movement_component.gd
@@ -1,32 +1,68 @@
 extends Node
 
-@export var speed : float = 300
-@export var acceleration : float = 3000
-@export var deceleration : float = 3000
-var parent : CharacterBody2D
+var parent : Player
+
+var is_moving : bool = false
 
 signal on_move(direction: Vector2)
 
-# Called when the node enters the scene tree for the first time.
+var grid_position : Vector2i = Vector2i(0, 2) :
+	set(value):
+		if is_moving:
+			return
+		
+		grid_position = value
+		update_position()
+
 func _ready():
 	if parent == null:
 		var new_parent = get_parent()
-		if new_parent is CharacterBody2D:
+		if new_parent is Player:
 			parent = new_parent
 		else:
 			assert(false, "MovementComponent must be a child of a CharacterBody2D node.")
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	on_move.emit(direction)
-	direction = direction.normalized()
-	if direction.x **2 + direction.y **2 > 0.4:
+	var x_direction: float = Input.get_axis("left", "right")
+	var y_direction: float = Input.get_axis("up", "down")
+	if x_direction **2 + y_direction **2 > 0.4:
 		GameGlobal.is_game_have_start = true
+	else:
+		return
 	
-	parent.velocity.x = move_toward(parent.velocity.x, direction.x * speed, acceleration * delta)
-	parent.velocity.y = move_toward(parent.velocity.y, direction.y * speed, acceleration * delta)
-	parent.move_and_slide()
-	pass
+	var move_direction: Vector2i = Vector2i.ZERO
+	
+	if x_direction > 0.4:
+		move_direction = Vector2i(1, 0)
+	elif x_direction < -0.4:
+		move_direction = Vector2i(-1, 0)
+
+	if y_direction > 0.4:
+		move_direction = Vector2i(0, 1)
+	elif y_direction < -0.4:
+		move_direction = Vector2i(0, -1)
+
+	on_move.emit(move_direction)
+	grid_position += move_direction
+
+
+func update_position():
+	var map = GameGlobal.map
+
+	var target_position = grid_position * map.tile_size + (map.tile_size / 2) + Vector2i(0, map.tile_size.y / 4)
+	if get_tree():
+		translation_animation(target_position)
+	else:
+		parent.position = target_position
+
+func translation_animation(target_position: Vector2) -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(parent, "position", target_position, 0.3)
+	tween.tween_callback(stop_movement)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	is_moving = true
+
+func stop_movement() -> void:
+	is_moving = false
+	
