c1840ba3152ebd71e2ddd2c57f98d0c2dc5e8653
Guillame Tran <guillaume1.tran@epitech.eu>
2025-08-01 21:35:41 +0200

    add cursed

--- actors/tile/components/cursed_component.gd
+++ actors/tile/components/cursed_component.gd
@@ -5,12 +5,17 @@ var tile: Tile
 @export var area: Area2D
 
 func _ready():
-    tile = get_parent() as Tile
-    area.body_exited.connect(_on_exit)
-    if tile == null:
-        print("CursedComponent: Parent is not a Tile")
-        return
+	tile = get_parent()
+	if not (tile is Tile):
+		print("CursedComponent: Parent is not a Tile")
+		return
+	area.body_exited.connect(_on_exit)
 
-func _on_exit():
-    tile.transform_to_another_type(load("res://actors/tile/full.tscn"), false)
-    
+func _on_exit(body: Node) -> void:
+	if not (body is Player):
+		return
+	if tile and tile.is_transform_to_full():
+		return
+	print("CursedComponent: Transforming tile to full")
+	tile.transform_to_another_type(load("res://actors/tile/full.tscn"), true)
+	
