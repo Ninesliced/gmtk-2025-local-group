153ab08087dc56a77a19e279157a0c09e50c4f62
notgoyome <guillaume1.tran@epitech.eu>
2025-08-02 10:17:29 +0200

    fix randomize

--- actors/tile/randomize_sprite.gd
+++ actors/tile/randomize_sprite.gd
@@ -7,8 +7,11 @@ func _ready() -> void:
 	var parent = get_parent()
 	if !(parent is AnimatedSprite2D):
 		return
+	sprite = parent
 	play_random_sprite()
 
 func play_random_sprite():
+	if !sprite:
+		return
 	var frame_count = sprite.sprite_frames.get_frame_count(sprite.animation)
 	sprite.frame = randi_range(0, frame_count)
