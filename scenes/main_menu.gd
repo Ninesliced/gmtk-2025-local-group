22cd2831936569f35ec047f42686cbb36a17360c
Guillame Tran <guillaume1.tran@epitech.eu>
2025-08-02 17:14:45 +0200

    Add movement component retrieval and enhance touch control functionality

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @export var main_menu_scene: PackedScene
 
 func _on_play_pressed():
+    print("Play button pressed")
     TransitionManager.change_scene(main_menu_scene, "circle_gradient", null, 1.0)
     pass # Replace with function body.
 
