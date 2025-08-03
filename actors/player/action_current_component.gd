7480db3124888fad6013a379b59f3d7d27f5e6e5
TheodoreBillotte <theodore.billotte@epitech.eu>
2025-08-03 14:00:01 +0200

    feat: update action UI and improve mouse position handling

--- actors/player/action_current_component.gd
+++ actors/player/action_current_component.gd
@@ -14,7 +14,12 @@ func _ready() -> void:
 	set_current_action()
 
 func _process(_delta: float) -> void:
-	pass
+	var pos_viewport: Vector2 = get_viewport().get_mouse_position()
+	var cam := get_parent().get_parent().get_node("Camera2D") as Camera2D
+	var world_pos: Vector2 = get_viewport().get_canvas_transform().affine_inverse() * pos_viewport
+	action_ui.global_position = world_pos - Vector2(8, 8)
+	
+
 func set_current_action():
 	if GameGlobal.action_stacks.size() == 0:
 		action_ui.set_texture_rect(null)
